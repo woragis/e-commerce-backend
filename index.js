0a7209d680044082600458fb02f6512f7ecda48e
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2023-11-21 23:19:13 -0300

    Init project
    Create index.js file, and install bcrypt, express, mongoose, connect-mongo, dotenv

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const port = 5000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.listen(port, () => console.log(`Server running on port ${port}`));
