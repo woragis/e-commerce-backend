c0dcfaf8cab128f9e4de7965c4295bc5babfe99f
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2023-12-07 01:48:16 -0300

    Install more packages

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const port = 5000;
 
-app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello e-commerce");
+});
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
