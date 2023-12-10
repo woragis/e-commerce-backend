7a27092b62f6452db3f89f86ccaa09c73b2215fe
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2023-12-10 16:06:47 -0300

    Create Product schema prototype for graphql

---
+++ src/graphql/schemas/ProductSchema.ts
@@ -0,0 +1,68 @@
+import { buildSchema } from 'graphql';
+
+const productSchema = buildSchema(`#graphql
+  type Query {
+    products: [Product!]!
+    product: Product!
+  }
+
+  type Mutation {
+    addProduct(input: addProductInput): Product!
+    editProduct(input: editProductInput): Product!
+    deleteProduct(id: ID!): Product
+  }
+
+  type Product {
+    name: String!
+    price: Number!
+    discount: Number
+    description: String!
+    technicalInfo: TechnicalInfo!
+    imageUrls: [String!]!
+    quantity: Number!
+    reviews: [Review!]!
+  }
+
+  type TechnicalInfo {
+    weight: Number!
+    dimensions: {
+      height: Number!
+      width: Number!
+      depth: Number!
+    }
+  }
+
+  input addProductInput {
+    name: String!
+    price: Number!
+    discount: Number
+    description: String!
+    technicalInfo: {
+      weight: Number!
+      dimensions: {
+        height: Number!
+        width: Number!
+        depth: Number!
+      }
+    }
+    imageUrls: [String!]!
+    quantity: Number!
+  }
+
+  input editProductInput {
+    name: String
+    price: Number
+    discount: Number
+    description: String
+    technicalInfo: {
+      weight: Number
+      dimensions: {
+        height: Number
+        width: Number
+        depth: Number
+      }
+    }
+    imageUrls: [String]!
+    quantity: Number
+  }
+`);
