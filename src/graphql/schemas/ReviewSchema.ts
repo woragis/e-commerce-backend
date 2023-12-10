50588f3778ff7b722c757d52de2b36e22da1c7ae
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2023-12-10 16:10:30 -0300

    Create review graphql schema protoype

---
+++ src/graphql/schemas/ReviewSchema.ts
@@ -0,0 +1,35 @@
+import { buildSchema } from 'graphql';
+
+const reviewSchema = buildSchema(`#graphql
+  type Query {
+    reviews: [Review]!
+    review(id: ID!): Review!
+  }
+  type Mutation {
+    addReview(input: addReviewInput): Review!
+    editReview(input: editReviewInput): Review!
+    deleteReview(id: ID!): Review
+  }
+
+  type Review {
+    userId: ID!
+    productId: ID!
+    rating: Number!
+    comment: String!
+  }
+
+  input addReviewInput {
+    userId: ID!
+    productId: ID!
+    rating: Number!
+    comment: String!
+  }
+
+  input editReviewInput {
+    userId: ID
+    productId: ID
+    rating: Number
+    comment: String
+  }
+  
+`);
