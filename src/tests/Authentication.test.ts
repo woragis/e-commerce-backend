6e80dfc000e17b0ceee8ada656fa0378077ba94d
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2023-12-12 23:39:08 -0300

    Init unit testing set up

---
+++ src/tests/Authentication.test.ts
@@ -0,0 +1,5 @@
+describe('Authentication Tests', () => {
+  test('Register test', async () => {});
+  test('Login test', async () => {});
+  test('Logout test', async () => {});
+});
---
+++ src/tests/Graphql.test.ts
@@ -0,0 +1,42 @@
+import { graphql } from 'graphql';
+import schema from '../schema/schemas';
+
+describe('User tests', () => {
+  test('Get users', async () => {});
+
+  test('Create user', async () => {
+    const createUserTest = `#graphql
+    mutation {
+      createUser(input: {name: "Jezreel de Andrade", username: "woragis", email: "jezreel.veloso@gmail.com", password: "2004"}) {
+        name
+        username
+        email
+        password
+      }
+    }
+  `;
+    const usersResult = await graphql(schema, createUserTest);
+    const { data } = usersResult;
+    expect(data).toEqual({ id: '1', name: 'John Doe' });
+  });
+
+  test('Get user', async () => {});
+  test('Update user', async () => {});
+  test('Delete user', async () => {});
+});
+
+describe('Product tests', () => {
+  test('Get Products', async () => {});
+  test('Get Product', async () => {});
+  test('Add Product', async () => {});
+  test('Edit Product', async () => {});
+  test('Delete Product', async () => {});
+});
+
+describe('Review Tests', () => {
+  test('Get Product Reviews', async () => {});
+  test('Get Product Review', async () => {});
+  test('Add Product Review', async () => {});
+  test('Edit Product Review', async () => {});
+  test('Delete Product Review', async () => {});
+});
