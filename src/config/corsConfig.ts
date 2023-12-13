4eb41605880b352bf441d3bcffebce3476136276
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2023-12-13 18:07:41 -0300

    Fix type bugs

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -6,12 +6,12 @@ import { CorsOptions } from 'cors';
 const corsOptions: CorsOptions = {
   origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : [],
   allowedHeaders: process.env.CORS_ALLOWED_HEADERS ? process.env.CORS_ALLOWED_HEADERS.split(',') : [],
-  credentials: process.env.CORS_CREDENTIALS || true,
+  credentials: Boolean(process.env.CORS_CREDENTIALS) || true,
   exposedHeaders: process.env.CORS_EXPOSED_HEADERS ? process.env.CORS_EXPOSED_HEADERS.split(',') : [],
-  maxAge: process.env.CORS_MAX_AGE || 600,
+  maxAge: Number(process.env.CORS_MAX_AGE) || 600,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  optionsSuccessStatus: process.env.CORS_OPTIONS_SUCCESS_STATUS || 204,
-  preflightContinue: process.env.CORS_PREFLIGHT_CONTINUE || false,
+  optionsSuccessStatus: Number(process.env.CORS_OPTIONS_SUCCESS_STATUS) || 204,
+  preflightContinue: Boolean(process.env.CORS_PREFLIGHT_CONTINUE) || false,
 };
 
 export default corsOptions;
--- src/config/sessionConfig.ts
+++ src/config/sessionConfig.ts
@@ -1,15 +1,14 @@
 import dotenv from 'dotenv';
 const env: string = process.env.NODE_ENV || 'development';
 dotenv.config({ path: 'src/config/.env.' + env });
-import session from 'express-session';
 import { createClient } from 'redis';
 import connectRedis from 'connect-redis';
 
 const redisClient = createClient({
-  password: process.env.REDIS_PASSWORD,
+  password: process.env.REDIS_PASSWORD || 'VVJ4TwicsgpYgE6XDUAImZ5nzsogch9w',
   socket: {
-    host: process.env.REDIS_SOCKET_HOST,
-    port: process.env.REDIS_SOCKET_PORT,
+    host: process.env.REDIS_SOCKET_HOST || 'redis-11306.c308.sa-east-1-1.ec2.cloud.redislabs.com',
+    port: Number(process.env.REDIS_SOCKET_PORT) || 11306,
   },
 });
 
@@ -17,13 +16,13 @@ const store = new connectRedis({ client: redisClient });
 
 const sessionOptions = {
   store,
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || 'woGiveUpBird',
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.SESSION_COOKIE_SECURE,
-    httpOnly: process.env.SESSION_COOKIE_HTTP_ONLY,
-    maxAge: process.env.SESSION_COOKIE_MAX_AGE,
+    secure: Boolean(process.env.SESSION_COOKIE_SECURE) || false,
+    httpOnly: Boolean(process.env.SESSION_COOKIE_HTTP_ONLY) || true,
+    maxAge: Number(process.env.SESSION_COOKIE_MAX_AGE) || 1800000,
   },
 };
 
