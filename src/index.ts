499bfaf71145c398a894aeae0f1614f9fd2ce4f1
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2023-12-13 21:18:47 -0300

    mend

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
+import dotenv from 'dotenv';
+const env: string = 'dev';
+dotenv.config({ path: __dirname + '/config/.env.' + env });
 import app from './app';
 import connectDatabase from './db';
-import dotenv from 'dotenv';
-const env: string = process.env.NODE_ENV || 'development';
-dotenv.config({ path: 'src/config/.env.' + env });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log('Express Server Running on port ' + PORT);
