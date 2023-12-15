51660709722041dff452cd15f115c61fbec5692a
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2023-12-15 01:38:52 -0300

    Fix product mutation requests
    Fix Add and Edit product, unfortunately edit still has a bug, that is: the object gets returned before changing so the response is the old object despite it already be different

--- src/schema/resolvers/ProductResolver.ts
+++ src/schema/resolvers/ProductResolver.ts
@@ -1,71 +1,80 @@
 import ProductModel from '../../models/ProductModel';
 import { GraphqlContext, GraphqlParent, SearchOptions } from '../../types/Server.type';
 import { addProductArgs, deleteProductArgs, editProductArgs, readProductArgs } from '../../types/Product.type';
-import { Review, readReview } from '../../types/Review.type';
+import { Review, readReviewArgs } from '../../types/Review.type';
 import ReviewModel from '../../models/ReviewModel';
 
 const productResolvers = {
   Query: {
-    products: async (parent: any, args: SearchOptions) => {
-      const products = await ProductModel.find().skip(args.offset).limit(args.limit);
+    products: async (parent: any, { offset, limit }: SearchOptions) => {
+      const products = await ProductModel.find().skip(offset).limit(limit);
+      console.log(products);
       return products;
     },
     product: async (parent: any, args: readProductArgs) => {
       const product = await ProductModel.findById(args._id);
+      console.log(product);
       return product;
     },
   },
   Mutation: {
     addProduct: async (parent: any, args: addProductArgs) => {
-      const newProduct = new ProductModel<addProductArgs>({
-        name: args.name,
-        price: args.price,
-        discount: args.discount,
-        discount_price: args.discount_price,
-        quantity: args.quantity,
-        description: args.description,
-        images: args.images,
-        specs: args.specs,
-      }).save();
-      // await newProduct.save();
-      return newProduct;
-      // newProduct
-      //   .save()
-      //   .then(savedProduct => {
-      //     return savedProduct;
-      //   })
-      //   .catch((error: any) => {
-      //     console.error('Error saving product ' + error);
-      //   });
+      try {
+        const newProduct = new ProductModel({
+          name: args.input.name,
+          price: args.input.price,
+          discount: args.input.discount,
+          discount_price: args.input.discount_price,
+          quantity: args.input.quantity,
+          description: args.input.description,
+          images: args.input.images,
+          specs: args.input.specs,
+        });
+        await newProduct.save();
+        return newProduct;
+      } catch (error) {
+        console.log('Error adding product ' + error);
+        return 'Error adding product ' + error;
+      }
     },
     editProduct: async (parent: any, args: editProductArgs) => {
-      const editedProduct = await ProductModel.findByIdAndUpdate(args._id, {
-        name: args.name,
-        price: args.price,
-        discount: args.discount,
-        discount_price: args.discount_price,
-        quantity: args.quantity,
-        description: args.description,
-        images: args.images,
-        specs: args.specs,
-      });
-      if (editedProduct) {
-        editedProduct
-          .save()
-          .then(product => {
-            return product;
-          })
-          .catch((error: any) => {
-            console.log('Error updating product ' + error);
-          });
-      } else {
-        return null;
+      try {
+        const editedProduct = await ProductModel.findByIdAndUpdate(args.input._id, {
+          name: args.input.name,
+          price: args.input.price,
+          discount: args.input.discount,
+          discount_price: args.input.discount_price,
+          quantity: args.input.quantity,
+          description: args.input.description,
+          images: args.input.images,
+          specs: args.input.specs,
+        });
+        // .then(
+        //   async product => {
+        //     await product?.save();
+        //     return product;
+        //   },
+        //   reason => {
+        //     console.error('Edit product rejected ' + reason);
+        //   }
+        // );
+        if (editedProduct) {
+          return editedProduct;
+        }
+      } catch (error) {
+        console.log('Error updating product ' + error);
+        return 'Error updating product ' + error;
       }
     },
     deleteProduct: async (parent: any, args: deleteProductArgs) => {
-      const deletedProduct = await ProductModel.findByIdAndDelete(args._id);
-      if (deletedProduct.ok) {
-        return 'ok';
+      try {
+        const deletedProduct = await ProductModel.findByIdAndDelete(args._id);
+        if (deletedProduct.ok) {
+          return 'Deleted Product';
+        }
+      } catch (error) {
+        console.error('Error deleting user ' + error);
+        return 'Error deleting user ' + error;
       }
     },
   },
@@ -76,7 +85,7 @@ const productResolvers = {
         .limit(args.limit);
       return reviews;
     },
-    productReview: async (parent: GraphqlParent, args: readReview) => {
+    productReview: async (parent: GraphqlParent, args: readReviewArgs) => {
       const review = await ReviewModel.findOne({ _id: args._id, product_id: parent._id } as Review);
       return review;
     },
